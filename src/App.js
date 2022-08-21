3ecded14e6230bc9d1fb7c1f16fecf435ccf35a6
Moises <moises-vd@hotmail.com>
2022-08-21 15:05:03 -0400

    proyect final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cards from './components/Cards';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    < Header header="Galería de Imágenes con React"/>
+    < Cards url="https://cdn.pixabay.com/photo/2017/04/05/01/16/palm-trees-2203737_960_720.jpg" title=" Puesta de Sol " description="Ubicación: Miami"  />
+    < Cards url="https://cdn.pixabay.com/photo/2017/08/29/09/26/disney-2692578_960_720.jpg" title=" Disney World " description="Ubicación: Orlando"  />
+    < Cards url="https://cdn.pixabay.com/photo/2016/08/16/17/32/hollywood-sign-1598473_960_720.jpg" title=" Cartel Gigante Hollywood " description="Ubicación: Los Ángeles"  />
+    < Footer />
+    </>
   );
 }
 
